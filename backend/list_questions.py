ccba1b4c5ef86a888af2fa776db2a3a5da236944
AbdulRehman <abdulrehmanidrees6776@gmail.com>
2026-05-05 00:49:31 +0500

    final directory clean-up

--- backend/list_questions.py
+++
@@ -1,9 +0,0 @@
-import json
-from pathlib import Path
-
-f = Path('data/questionsAnswers.json')
-data = json.load(open(f))
-q_list = data.get('Software Engineer', [])
-
-for q in q_list[:12]:
-    print(f"Q{q['question_id']}: {q['question'][:70]}")
